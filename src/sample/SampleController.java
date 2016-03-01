9ab55bb2b704e373d7c29b2937b643f45b44547f
pog-pp <pog-pp@yandex.ru>
2016-03-01 22:18:48 +0300

    JavaFX Sample

---
+++ src/sample/SampleController.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController {
+    public Label helloWorld;
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
